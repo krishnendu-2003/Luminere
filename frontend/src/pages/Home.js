80ffb766d1929c221cf2df45031de404f884368e
krishnendu-2003 <skrishnendu115@gmail.com>
2024-09-13 15:32:28 +0530

    Updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import './Home.css'
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
+    <div className='Main-Container'> {/* Main container, will contain the black color */}
+        <div className='Home-s1'>
+        <h2 className='Home-s1-alpha'>WE DON'T JUST MANAGE <br/> FEEDS, WE CRAFT <br/> OBSESSIONS.</h2>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        </div>
   );
 }
 
