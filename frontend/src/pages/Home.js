4a5214e7f02761eda80ea8762adc91c5df03f010
krishnendu-2003 <skrishnendu115@gmail.com>
2024-09-13 21:18:04 +0530

    Updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 import logo1 from '../assets/h-s1-img.png';
 function Home() {
@@ -12,18 +13,24 @@ function Home() {
     <div className='Main-Container'> {/* Main container, will contain the black color */}
         <div className='Home-s1'>
         <h2 className='Home-s1-alpha'>WE DON'T JUST MANAGE <br/> FEEDS, WE CRAFT <br/> OBSESSIONS.</h2>
+        <Link to="/contact">
         <button className='button-s1' onClick={handleClick}><p>Let's Collaborate</p></button>
+        </Link>
         </div>
         
         <div>
         <div className='Home-s2'>
+            <div className='Home-s2-p1-whole'>
                 <div className='Home-s2-p1'>
+                    <div>
                     <p>This is the space to introduce visitors to the <br/> business or brand. Briefly explain who's behind <br/> it, what it does and what makes it unique. Share <br/> its core values and what this site has to offer. </p>
-                    <img src={logo1}/>
+                    <Link to="/about">
+                    <button>Learn More About Us</button>
+                    </Link>
+                    </div>
+                    <img src={logo1}/>        
                 </div>
-            
-            <div>
-                <p>Learn More About Us</p>
+                
             </div>
             <div> 
                 {/* Will contain the blank space */}
