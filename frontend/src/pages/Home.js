b42aa1ac07f4256a0c9097451efd58949257c3ab
krishnendu-2003 <skrishnendu115@gmail.com>
2024-09-13 15:49:17 +0530

    Update

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Home.css'
 function Home() {
+
+    const handleClick = () => {
+        // Do something when the button is clicked
+        console.log('Button clicked!');
+      };
+
   return (
     <div className='Main-Container'> {/* Main container, will contain the black color */}
         <div className='Home-s1'>
         <h2 className='Home-s1-alpha'>WE DON'T JUST MANAGE <br/> FEEDS, WE CRAFT <br/> OBSESSIONS.</h2>
+        <button className='button-s1' onClick={handleClick}><p>Let's Collaborate</p></button>
         </div>
         <div>
 
