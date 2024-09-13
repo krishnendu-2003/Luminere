f6c4be08e6b9fba07978d14361fbc12346c8d4e2
krishnendu-2003 <skrishnendu115@gmail.com>
2024-09-14 00:03:15 +0530

    Updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -77,13 +77,19 @@ function Home() {
         <div className='Home-s4'>
             <div className='Home-s4-p1'>
                 <div className='Home-s4-p1-left'>
-                    <div>
+                    <div className='Home-s4-p1-left-001'>
                         <h4>GET IN TOUCH</h4>
                     </div>
-                    <div>
-                        <h4>info@mysite.com</h4>
+                    <div className='Home-s4-p1-left-002'>
+                    {/* <img src={require('../assets/Mail.png')} alt='location'/> */}
+                        {/* <h4>info@mysite.com</h4> */}
+                        <a href='mailto:info@mysite.com' className='email-link'>
+                        <img src={require('../assets/Mail.png')} alt='location' className='Home-s4-p1-left-002-img'/>
+                        <h4 className='Home-s4-p1-left-002-h4'>info@mysite.com</h4>
+                        </a>
                     </div>
-                    <div>
+                    <div  className='Home-s4-p1-left-003'>
+                        <img src={require('../assets/Location.png')} alt='location'/>
                         <h4>address</h4>
                     </div>
                 </div>
