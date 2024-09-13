4e0fabb86f34af911cbd2be4dd4c80fdd1ff88f4
krishnendu-2003 <skrishnendu115@gmail.com>
2024-09-13 23:24:54 +0530

    Updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import logo from '../assets/Lum_nereLogo1.png';
 import logo1 from '../assets/h-s1-img.png';
 import logo2 from '../assets/h4b-logo.png';
 import logo3 from '../assets/mnzi-logo.png';
@@ -27,7 +28,7 @@ function Home() {
             <div className='Home-s2-p1'>
                     <div className='Home-s2-p1-5'>
                     <p>This is the space to introduce visitors to the <br/> business or brand. Briefly explain who's behind <br/> it, what it does and what makes it unique. Share <br/> its core values and what this site has to offer. </p>
-                    <img src={logo1}/>  
+                    <img src={logo1} alt='Luminere logo'/>  
                     </div>
                     <div>
                     <Link to="/about" className='learn-about'>
@@ -74,6 +75,24 @@ function Home() {
         </div>
         
         <div className='Home-s4'>
+            <div className='Home-s4-p1'>
+                <div className='Home-s4-p1-left'>
+                    <div>
+                        <h4>GET IN TOUCH</h4>
+                    </div>
+                    <div>
+                        <h4>info@mysite.com</h4>
+                    </div>
+                    <div>
+                        <h4>address</h4>
+                    </div>
+                </div>
+                <div className='Home-s4-p1-right'>
+                    <img src={logo} alt='Luminere logo'/>  
+                    <p>We're always curious about the next <br/> problem we can tackle or a new <br/> challenge we can embrace. So we'd love <br/> to hear from you. Send us a message or <br/> give us a call to see how we can help <br/> you.</p>
+                </div>
+
+            </div>
 
         </div>
         </div>
